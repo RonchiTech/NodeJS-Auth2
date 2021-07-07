e6d3b2b7377bcea52ca6a8471982bd784c6a467f
ronchitech <ronchibusiness@gmail.com>
2021-07-07 23:07:58 +0800

    using stripe in app

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.use('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
+  console.log(error);
   // res.status(error.httpStatusCode).render('/')
   // res.redirect('/500');
   res.status(500).render('500', {
